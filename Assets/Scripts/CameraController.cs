d9295b55dbcf78198978a9261c20fcc95e2163ee
Michael Bonessi <mbonessi@uncc.edu>
2019-01-31 19:21:45 -0500

    Added player movement/mouse control

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector2 lookDirection;
+    Vector2 smoothingVector;
+    GameObject player;
+
+    [SerializeField]
+    float sensitivity;
+
+    [SerializeField]
+    float smoothing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = transform.parent.gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraMovement();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    void CameraMovement()
+    {
+        Vector2 mouseDirection = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouseDirection = Vector2.Scale(mouseDirection, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothingVector.x = Mathf.Lerp(smoothingVector.x, mouseDirection.x, 1f / smoothing);
+        smoothingVector.y = Mathf.Lerp(smoothingVector.y, mouseDirection.y, 1f / smoothing);
+        lookDirection += smoothingVector;
+        lookDirection.y = Mathf.Clamp(lookDirection.y, -50f, 0); //Limit the vertical mouse movement
+        if(Cursor.lockState == CursorLockMode.Locked)
+        {
+            transform.localRotation = Quaternion.AngleAxis(-lookDirection.y, Vector3.right);
+            player.transform.localRotation = Quaternion.AngleAxis(lookDirection.x, player.transform.up);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed;
+    Vector3 movementInput;
+    Rigidbody rb;
+    [SerializeField] Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5f;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        AnimatePlayer();
+    }
+
+    void MovePlayer()
+    {
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            movementInput = new Vector3(Input.GetAxisRaw("Horizontal") * speed, 0f, Input.GetAxisRaw("Vertical") * speed);
+            movementInput = transform.TransformDirection(movementInput);
+            rb.velocity = movementInput;
+        }
+    }
+
+    void AnimatePlayer()
+    {
+        if(Cursor.lockState == CursorLockMode.Locked)
+        {
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+            {
+                anim.SetBool("Moving", true);
+            }
+            else
+            {
+                anim.SetBool("Moving", false);
+            }
+        }
+        
+    }
+}

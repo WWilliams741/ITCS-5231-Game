754f841ef70bcd25daf2a9f0080834fb50f91c88
russell8950 <russell.rigsbee@gmail.com>
2019-03-13 19:46:10 -0400

    Added Music Manager
    Added music to the game application

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource audio;
+    [SerializeField] private AudioClip clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio.Play();
+    }
+
+    // Update is called once per frame
+}

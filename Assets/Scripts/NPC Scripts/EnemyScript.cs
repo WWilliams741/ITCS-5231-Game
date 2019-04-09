e00ada8293bb9c971a532a7f7d412e3caafd034f
DRBEES741 <wwilli47@uncc.edu>
2019-04-09 11:57:24 -0400

    Enemy Patrol coded
    Could use some adjustments

--- Assets/Scripts/NPC Scripts/EnemyScript.cs
+++ Assets/Scripts/NPC Scripts/EnemyScript.cs
@@ -1,16 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyScript : MonoBehaviour
 {
     public NPCData sourceData;
     [SerializeField] private Animator anim;
+    private NavMeshAgent agent;
+    private bool patrolling;
+    private Vector3 theDestination;
     public int health;
+    private float radiusofSatisfaction;
 
     private void Start()
     {
+        patrolling = false;
+        radiusofSatisfaction = 1f;
         health = sourceData.vitality;
+        agent = GetComponent <NavMeshAgent> ();
+    }
+
+    private void Update()
+    {
+
+        //Plan on coding for enemy detection of player - different for boss (or just set radius of detection to be whole map lol
+
+
+        if (!patrolling)
+        {
+            patrolling = true;
+            theDestination = Patrol();
+            agent.SetDestination(theDestination);
+        }
+        else if (Vector3.Distance(theDestination, transform.position) <= radiusofSatisfaction)
+        {
+            patrolling = false;
+        }
     }
 
     public void TakeDamage(int damage)
@@ -23,5 +49,13 @@ public class EnemyScript : MonoBehaviour
         }
         
         Debug.Log("Enemy Health is now: " + health);
+    }
+
+    public Vector3 Patrol()
+    {
+        Vector2 circlePoint = Random.insideUnitCircle * 5;
+        Vector3 newPoint = new Vector3(circlePoint.x, gameObject.transform.position.y, circlePoint.y);
+
+        return newPoint;
     }
 }

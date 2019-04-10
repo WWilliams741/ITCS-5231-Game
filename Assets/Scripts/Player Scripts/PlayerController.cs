ffbc8708d68e90edb7f35b31d53f3c43234b29eb
DRBEES741 <wwilli47@uncc.edu>
2019-04-10 15:41:45 -0400

    fixed the attacking animations
    - has exit time is a fucking dick

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -107,10 +107,10 @@ public class PlayerController : MonoBehaviour
                 }
                 
             }
-            if (Input.GetMouseButtonUp(0))
-            {
-                anim.SetBool("Attacking", false);
-            }
+            //if (Input.GetMouseButtonUp(0))
+            //{
+            //    anim.SetBool("Attacking", false);
+            //}
             if (Input.GetMouseButtonDown(1) && !anim.GetBool("Attacking"))
             {
                 blocking = true;
@@ -216,6 +216,7 @@ public class PlayerController : MonoBehaviour
 
     public void Attacking()
     {
+        print("setting attack back to false");
         anim.SetBool("Attacking", false);
     }
 

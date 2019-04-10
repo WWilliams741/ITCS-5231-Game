c8f4ad713a1d5862ca386b7e86bbe30164f4cc72
DRBEES741 <wwilli47@uncc.edu>
2019-04-10 17:22:50 -0400

    EnemyScript Changes
    - Thank fucking God this shit works now. Fuck GitHub and you reading this - just because.

--- Assets/Scripts/NPC Scripts/EnemyScript.cs
+++ Assets/Scripts/NPC Scripts/EnemyScript.cs
@@ -12,24 +12,65 @@ public class EnemyScript : MonoBehaviour
     private Vector3 theDestination;
     public int health;
     private float radiusofSatisfaction;
+    private float radiusofPatrol;
+    private Vector3 startPoint;
+    private Vector3 vision;
+    private bool spotted;
+    private GameObject theEnemy;
 
     private void Start()
     {
+        spotted = false;
         patrolling = false;
         radiusofSatisfaction = 1f;
+        radiusofPatrol = 20f;
         health = sourceData.vitality;
-        agent = GetComponent <NavMeshAgent> ();
+        agent = GetComponent<NavMeshAgent>();
+        startPoint = transform.position;
+        vision = transform.forward;
     }
 
     private void Update()
     {
-
+        vision = transform.forward;
+        RaycastHit hit;
+        Ray ray = new Ray(new Vector3(transform.position.x, 1.5f, transform.position.z), vision);
         //Plan on coding for enemy detection of player - different for boss (or just set radius of detection to be whole map lol
-
-
-        if (!patrolling)
+        if (Physics.Raycast(ray, out hit, 10f))
         {
-            patrolling = true;
+            if (hit.collider.gameObject.tag.Equals("Player"))
+            {
+                spotted = true;
+                theEnemy = hit.collider.gameObject;
+            }
+        }
+
+        Debug.DrawRay(new Vector3(transform.position.x, 1.5f, transform.position.z), vision * 10f, Color.red);
+
+        if (spotted)
+        {
+            agent.SetDestination(theEnemy.transform.position);
+
+            if (anim.name.Equals("Boss Controller"))
+            {
+                if (Vector3.Distance(transform.position, theEnemy.transform.position) <= 2f)
+                {
+                    anim.SetBool("Attacking", true);
+                    anim.SetInteger("Attack", Random.Range(0, 3));
+                }
+            }
+            else
+            {
+                if (Vector3.Distance(transform.position, theEnemy.transform.position) <= 2f)
+                {
+                    anim.SetBool("Attacking", true);
+                    anim.SetInteger("Attack", Random.Range(0, 2));
+                }
+            }
+        }
+
+        if (!patrolling && !spotted)
+        {
             theDestination = Patrol();
             agent.SetDestination(theDestination);
         }
@@ -42,12 +83,12 @@ public class EnemyScript : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
-        if(health <= 0)
+        if (health <= 0)
         {
             anim.SetTrigger("Die");
             GetComponent<Collider>().enabled = false;
         }
-        
+
         Debug.Log("Enemy Health is now: " + health);
     }
 
@@ -56,6 +97,14 @@ public class EnemyScript : MonoBehaviour
         Vector2 circlePoint = Random.insideUnitCircle * 5;
         Vector3 newPoint = new Vector3(circlePoint.x, gameObject.transform.position.y, circlePoint.y);
 
+        while (Vector3.Distance(newPoint, startPoint) > radiusofPatrol)
+        {
+            circlePoint = Random.insideUnitCircle * 5;
+            newPoint = new Vector3(circlePoint.x, gameObject.transform.position.y, circlePoint.y);
+
+        }
+
+        patrolling = true;
         return newPoint;
     }
 }

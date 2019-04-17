68d35f67b55bb491543aa81aa86b7164742629ca
russell8950 <russell.rigsbee@gmail.com>
2019-04-17 15:15:37 -0400

    Enemy HealthBar Rotation Fix

---
+++ Assets/Scripts/NPC Scripts/CanvasScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasScript : MonoBehaviour
+{
+    [SerializeField] private Camera enemyHealthBar;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 thePosition = enemyHealthBar.transform.position - transform.position;
+		
+		thePosition.x = thePosition.z = 0f;
+		transform.LookAt(enemyHealthBar.transform.position - thePosition);
+		transform.Rotate(0,180,0);
+    }
+}

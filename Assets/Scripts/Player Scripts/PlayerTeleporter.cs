7afddfca84b98659d4159f8cda70759c225f9d28
Michael Bonessi <mbonessi@uncc.edu>
2019-04-29 13:38:18 -0400

    Added player spawn point

---
+++ Assets/Scripts/Player Scripts/PlayerTeleporter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleporter : MonoBehaviour
+{
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        player.transform.position = transform.position;
+    }
+}
